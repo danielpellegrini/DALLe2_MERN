aa755100236632a6924f70c3ab1c567071d68c33
Daniel Pellegrini <danielpellegrini87@gmail.com>
2023-04-03 13:47:43 +0200

    Start Node Server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+// pull environment variables from .env file
+dotenv.config();
+
+const app = express();
+// adding middlewares
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+// Root Route
+app.get('/', async (req, res) => {
+    res.send('Hello from DALL-E!');
+})
+
+const startServer = async () => {
+    app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
+}
+
+startServer();
