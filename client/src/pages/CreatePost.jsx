80794618ca060aa28c7d84f41ba299304983127b
Daniel Pellegrini <danielpellegrini87@gmail.com>
2023-04-03 10:48:18 +0200

    Started CreatePost Component

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,8 +6,51 @@ import { getRandomPrompt } from '../utils';
 import { FormField, Loader } from '../components';
 
 const CreatePost = () => {
+  const navigate = useNavigate(); // navigate back to the home page once the post is created
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: ''
+  });
+  const [generatingImg, setGeneratingImg] = useState(false); // useState used while making contact with the API and waiting to get back the image
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {};
+  
+  const handleChange = (e) => {};
+  
+  const handleSurpriseMe = () => {};
+
   return (
-    <div>CreatePost</div>
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>Create</h1>
+        <p className='mt-2 text-[#666e75] text-[16px] max-w-[500px]'>Create imaginative and visually stunning images generated through DALL-E AI and share them with the community!</p>
+      </div>
+
+      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-5'>
+          <FormField 
+            labelName="Your name"
+            type="text"
+            name="name"
+            placeholder="John Doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField 
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="Dracula walking down the street of New York City in the 1920s, black and white photography"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
+    </section>
   )
 }
 
