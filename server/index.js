0bfcd2840f1a86256f30836e3130c7d36d70685e
Daniel Pellegrini <danielpellegrini87@gmail.com>
2023-04-03 16:14:46 +0200

    dall-e route works good

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
 // pull environment variables from .env file
 dotenv.config();
@@ -12,9 +14,13 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
+// api endpoints that can be hooked on from client side
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
 // Root Route
 app.get('/', async (req, res) => {
-    res.send('Hello from DALL-E!');
+    res.send('Hello from index!');
 })
 
 const startServer = async () => {
